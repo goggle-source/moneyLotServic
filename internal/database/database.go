212b4f958db2fb588e9a252e211379453d215362
google_source <asssv0423348@gmail.com>
2026-07-01 15:10:56 +0300

    Delete log in all func
    removed unnecessary logs from the function because they were only a
    hidrance

--- internal/database/database.go
+++ internal/database/database.go
@@ -9,13 +9,11 @@ import (
 	"time"
 
 	"github.com/goggle-source/moneyLotServic/internal/config"
-	"github.com/goggle-source/moneyLotServic/internal/lib/logger"
 	"github.com/goggle-source/moneyLotServic/internal/models"
 )
 
 type DB struct {
-	log *slog.Logger
-	DB  *sql.DB
+	DB *sql.DB
 }
 
 func InitDB(log *slog.Logger, cfg *config.Cfg) (*DB, error) {
@@ -39,22 +37,15 @@ func InitDB(log *slog.Logger, cfg *config.Cfg) (*DB, error) {
 	db.SetConnMaxIdleTime(cfg.DB.ConnMaxIdle)
 	db.SetConnMaxLifetime(cfg.DB.ConnLifeTime)
 
-	return &DB{DB: db, log: log}, nil
+	return &DB{DB: db}, nil
 
 }
 
 func (d *DB) AddMoneyToUser(ctx context.Context, userID string, money float64) (bool, error) {
 	const op = "postgresql.AddToMoneyUser"
 
-	log := d.log.With(slog.String("op", op))
-
-	log.Info("start AddMoneyToUser")
-
-	log.Debug("data", slog.Float64("money", money), slog.String("userID", userID))
-
 	tx, err := d.DB.BeginTx(ctx, &sql.TxOptions{})
 	if err != nil {
-		log.Error("error create transaction", logger.Err(err))
 		return false, ValidationErrorPostgresql(err)
 	}
 
@@ -68,29 +59,21 @@ func (d *DB) AddMoneyToUser(ctx context.Context, userID string, money float64) (
 		userID, money)
 
 	if err != nil {
-		log.Error("error update userMoney", logger.Err(err))
 		return false, ValidationErrorPostgresql(err)
 	}
 
 	rowsAffected, err := res.RowsAffected()
 
-	log.Debug("rows", slog.Int("r", int(rowsAffected)))
-
 	if err != nil {
-		log.Error("error get rowsAffected", logger.Err(err))
 	}
 
 	if rowsAffected == 0 {
-		log.Error("error update userMoney", slog.Int("rows", int(rowsAffected)))
 		return false, ValidationErrorPostgresql(err)
 	}
 
 	if err = tx.Commit(); err != nil {
-		log.Error("error commit transaction", logger.Err(err))
 		return false, ValidationErrorPostgresql(err)
 	}
-
-	log.Info("success AddMoneyToUser")
 
 	return true, nil
 }
@@ -98,13 +81,8 @@ func (d *DB) AddMoneyToUser(ctx context.Context, userID string, money float64) (
 func (d *DB) ReduceMoneyToUser(ctx context.Context, userID string, money float64) (bool, error) {
 	const op = "postgresql.ReduceMoneyToUser"
 
-	log := d.log.With(slog.String("op", op))
-
-	log.Info("start ReduceMoneyToUser")
-
 	tx, err := d.DB.BeginTx(ctx, &sql.TxOptions{})
 	if err != nil {
-		log.Error("error create transaction", logger.Err(err))
 		return false, ValidationErrorPostgresql(err)
 	}
 
@@ -113,12 +91,10 @@ func (d *DB) ReduceMoneyToUser(ctx context.Context, userID string, money float64
 	err = d.DB.QueryRowContext(ctx, "SELECT userMoney FROM money WHERE userID = $1", userID).Scan(&userMoney)
 
 	if err != nil {
-		log.Error("error get userMoney", logger.Err(err))
 		return false, ValidationErrorPostgresql(err)
 	}
 
 	if userMoney < money {
-		log.Error("lack of funds")
 		return false, ErrLackOfFunds
 	}
 
@@ -131,14 +107,12 @@ func (d *DB) ReduceMoneyToUser(ctx context.Context, userID string, money float64
 		if errors.Is(err, sql.ErrNoRows) {
 			return false, fmt.Errorf("%s:%w", op, ErrNotFound)
 		}
-		log.Error("error update userMoney", logger.Err(err))
 		return false, ValidationErrorPostgresql(err)
 	}
 
 	rowsAffected, err := res.RowsAffected()
 
 	if err != nil {
-		log.Error("error get rowsAffected", logger.Err(err))
 		return false, ErrDatabase
 	}
 
@@ -147,20 +121,14 @@ func (d *DB) ReduceMoneyToUser(ctx context.Context, userID string, money float64
 	}
 
 	if err = tx.Commit(); err != nil {
-		log.Error("error commit transaction", logger.Err(err))
 		return false, ValidationErrorPostgresql(err)
 	}
 
-	log.Info("success ReduceMoneyToUser")
 	return true, nil
 }
 
 func (d *DB) GetMoneyToUser(ctx context.Context, userID string) (float64, error) {
 	const op = "postgresql.GetMoneyToUser"
-
-	log := d.log.With(slog.String("op", op))
-
-	log.Info("start GetMoneyToUser")
 
 	var allMoney float64
 
@@ -171,11 +139,8 @@ func (d *DB) GetMoneyToUser(ctx context.Context, userID string) (float64, error)
 		if errors.Is(err, sql.ErrNoRows) {
 			return 0, nil
 		}
-		log.Error("error get userMoney", logger.Err(err))
 		return 0, ValidationErrorPostgresql(err)
 	}
-
-	log.Info("success GetMoneyToUser")
 
 	return allMoney, nil
 }
@@ -183,18 +148,12 @@ func (d *DB) GetMoneyToUser(ctx context.Context, userID string) (float64, error)
 func (d *DB) HealthCheack(ctx context.Context) (models.HealthCheakDatabase, error) {
 	const op = "db.HealthCheack"
 
-	log := d.log.With(slog.String("op", op))
-
-	log.Info("start healthCheack")
-
 	start := time.Now()
 
 	_, err := d.DB.Exec("SELECT 1")
 	if err != nil {
-		log.Info("request execution error", logger.Err(err))
 		return models.HealthCheakDatabase{ConnDB: false, LeadTimeDB: time.Since(start)}, err
 	}
-	log.Info("success request to DB")
 
 	return models.HealthCheakDatabase{ConnDB: true, LeadTimeDB: time.Since(start)}, nil
 }
@@ -202,18 +161,12 @@ func (d *DB) HealthCheack(ctx context.Context) (models.HealthCheakDatabase, erro
 func (d *DB) CheckingIsUser(ctx context.Context, userID string, tx *sql.Tx) (bool, error) {
 	const op = "postgresql.CheckingIsUser"
 
-	log := d.log.With(slog.String("op", op))
-
-	log.Info("start checkingIsUser")
-
 	var isValueUser bool
 
 	err := tx.QueryRowContext(ctx, "SELECT EXISTS(SELECT 1 FROM money WHERE userID = $1)", userID).Scan(&isValueUser)
 	if err != nil {
-		log.Error("error get userID from database", logger.Err(err))
 		return false, err
 	}
-	log.Info("success checking an user")
 
 	return isValueUser, nil
 }
